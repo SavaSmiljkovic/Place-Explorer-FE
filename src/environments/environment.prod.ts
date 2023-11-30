ff3db8f6e29717901ceebde9afe70562f454abcb
SavaSmiljkovic <savasmiljkov@gmail.com>
2023-11-30 20:58:06 +0100

    CDT-04: environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+
+  allPlacesUrl: 'allPlaces',
+  singlePlaceUrl: 'singlePlace',
+
+  endpoint: 'http://127.0.0.1:8080/place-explorer/',
+  placesEndpoint: 'places/'
 };
